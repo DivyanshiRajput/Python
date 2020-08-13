407c2b2ae0dd6215b6c40ec0e57f76664167432b
Divyanshi Rajput <56169692+DivyanshiRajput@users.noreply.github.com>
2020-08-13 14:06:36 +0530

    Delete double.py

--- double.py
+++
@@ -1,9 +0,0 @@
-def double(l):
-## Your code - begin
-    return [x*2 for x in l] #using list comprehension for doubling the elements of the list.
-## Your code - end
-    
-if __name__ == "__main__":
-  l = [1, 2, 3]
-  print "input = ", l
-  print double([1, 2, 3])
